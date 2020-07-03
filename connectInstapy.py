c768f468bbecd3a8c2bef034c77e263289920960
damienPilat <dspilat@hotmail.com>
2020-07-03 11:40:03 +0200

    .env support

--- connectInstapy.py
+++ connectInstapy.py
@@ -6,5 +6,6 @@ from instapy import InstaPy
 session = InstaPy(username=os.getenv("USERNAME"), password=os.getenv("PASSWORD"))
 session.login()
 
+
 # Unfollow 400 accounts who dont follow back at random, sleep 10 min
 session.unfollow_users(amount=100, nonFollowers=True, style="RANDOM", unfollow_after=42*60*60, sleep_delay=655)
